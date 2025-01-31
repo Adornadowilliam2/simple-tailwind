d1a215ed66a45ee63f4b90596482f4bf1256ced5
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-01-31 20:00:18 +0800

    - adding new index js to teach how to use node

---
+++ index.js
@@ -0,0 +1,31 @@
+const msg ="Hello world"
+console.log(msg)
+
+const total = 12;
+
+const neo_total = 12+12;
+
+console.log(neo_total)
+
+const neo_eval = "12 + 12";
+console.log(eval(neo_eval))
+
+
+const arr =[1,2,3,4,5,6,7,8,9]
+
+arr.map((item) => {
+    console.log(item)
+})
+
+
+const isMale = false;
+
+if(isMale === true){
+    console.log("You're a Male");
+}else{
+    console.log("You're a Female")
+}
+
+
+
+
